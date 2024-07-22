0f227c1c9f39f1d6b6a573bd592f5b6ad2f045e9
cor22982 <cor22982@uvg.edu.gt>
2024-07-22 12:26:17 -0600

    feat: Verify parse

--- Lab2/ejercicio2/main.py
+++ Lab2/ejercicio2/main.py
@@ -1,11 +1,18 @@
 #Ejercicio 2 Laboratorio 2. 
-from parse import parsechain
+from parse import parsechain, determinate_balance
 
 
-cadena = "a(a|b)*b+a?"
-
-cadenas = [char for char in cadena]
-
-print(parsechain(cadenas))
-
+cadena = " a ( a | b ) * b + a ?"
+cadena2= " ( ( a | b ) b ) * [ az ] b ]"
+cadena3= " ( a * b * c * d * ( a | e | i | o | u ) ) e * f * g * h ) { 1 , 2 }"
+cadena4 = "^ [ aZ ] . com { 5 , 30 }"
+cadena5 = "( [ [ az ] [ AZ ] ] ( ( ( ( ( . | ; ) | ; ) | . ) | . ) | . ) { 10 , 20 } ) ∗ ) +"
+cadenas = cadena.split(" ")
+cadenas2 = cadena2.split(" ")
+cadenas3 = cadena3.split(" ")
+cadenas4 = cadena4.split(" ")
+cadenas5 = cadena5.split(" ")
+print(parsechain(cadenas5))
+cl = parsechain(cadenas5)
+determinate_balance(cl)
 
--- Lab2/ejercicio2/parse.py
+++ Lab2/ejercicio2/parse.py
@@ -4,13 +4,22 @@ def parsechain(cadena):
     i = 0
     while i < len(cadena):
         cd = cadena[i]
-        if cd == '(':
-            cadena_popup, indice = parsechain(cadena[i + 1:])
-            cadena_return.append(cadena_popup)
+        
+        if cd == '(' or cd == '[' or cd == '{':
+            cadena_restante, indice = parsechain(cadena[i + 1:])
+            cadena_return.append(cadena_restante)
             i += indice + 1
-        elif cd == ')':
+        elif cd == ')' or cd == ']' or cd == '}':
             return cadena_return, i
         else:
             cadena_return.append(cd)
         i += 1
     return cadena_return
+
+def determinate_balance(parse_chain):
+  #si esta balanceada no devolvera el indice donde se quedo , ya que este se devuelve si esta completamente cerrada.
+  # lo que hace que sea una lista si esta balanceada y una tupla si no. 
+  if isinstance(parse_chain, list):
+    print("La expresion regular esta balanceada")
+  elif isinstance(parse_chain, tuple):
+    print("La expresion regular no esta balanceada")
