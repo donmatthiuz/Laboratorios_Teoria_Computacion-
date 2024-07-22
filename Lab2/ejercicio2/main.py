22cb372798abe70823d3a5862c6dae7ba6de2f1f
cor22982 <cor22982@uvg.edu.gt>
2024-07-22 16:53:29 -0600

    feat: Chain read

--- Lab2/ejercicio2/main.py
+++ Lab2/ejercicio2/main.py
@@ -2,17 +2,16 @@
 from parse import parsechain, determinate_balance
 
 
-cadena = " a ( a | b ) * b + a ?"
-cadena2= " ( ( a | b ) b ) * [ az ] b ]"
-cadena3= " ( a * b * c * d * ( a | e | i | o | u ) ) e * f * g * h ) { 1 , 2 }"
-cadena4 = "^ [ aZ ] . com { 5 , 30 }"
-cadena5 = "( [ [ az ] [ AZ ] ] ( ( ( ( ( . | ; ) | ; ) | . ) | . ) | . ) { 10 , 20 } ) ∗ ) +"
-cadenas = cadena.split(" ")
-cadenas2 = cadena2.split(" ")
-cadenas3 = cadena3.split(" ")
-cadenas4 = cadena4.split(" ")
-cadenas5 = cadena5.split(" ")
-print(parsechain(cadenas5))
-cl = parsechain(cadenas5)
-determinate_balance(cl)
+def getExpresions_file (filename):
+  with open(filename, "r") as file:
+    lines = file.read().splitlines()
+    for line in lines:
+      cadena = line.split(" ")
+      parsed = parsechain(cadena)
+      print(parsed)
+      determinate_balance(parsed)
 
+getExpresions_file("file_reader.txt")
+
+cad = "( [ [ az ] [ AZ ] ] ( ( ( ( ( . | ; ) | ; ) | . ) | . ) | . ) { 10 , 20 } ) ∗ ) +"
+print(parsechain(cad.split(" ")))
