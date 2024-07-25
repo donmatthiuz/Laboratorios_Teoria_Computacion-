7693f2db8e8a625daf10be18276933352d258084
cor22982 <cor22982@uvg.edu.gt>
2024-07-24 19:33:40 -0600

    feat: Add formatedRegex

--- Lab2/ejercicio3/main.py
+++ Lab2/ejercicio3/main.py
@@ -1,1 +1,5 @@
 from shuntingYard import *
+
+regex = "a(b|c)*d"
+formatted_regex = formatRegEx(regex)
+print(formatted_regex)
--- Lab2/ejercicio3/shuntingYard.py
+++ Lab2/ejercicio3/shuntingYard.py
@@ -2,7 +2,7 @@ def getPrecedence (c):
   precedencias = {
     '(': 1,
     '|': 2,
-    '': 3,
+    ' ': 3,
     '?': 4,
     '*': 4,
     '+': 4,
@@ -11,9 +11,17 @@ def getPrecedence (c):
   return precedencias[c]
 
 def formatRegEx(regex):
+  res = ""
   allOperators = ['|', '?', '+', '*', '^']
   binaryOperators = ['^', '|']
   for i in range (len(regex)):
     c1 = regex[i]
     if ((i+1)<len(regex)):
       c2 = regex[i+1]
+      res += c1
+      if (c1 != '(' and c2 != ')' and (c2 not in allOperators) and (c1 not in binaryOperators)):
+        res += ' '
+  res += regex[-1]
+  return res.strip()
+
+
