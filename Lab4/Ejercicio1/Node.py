e14fa57d76618382d8616ee82e79f99cbea1b98f
cor22982 <cor22982@uvg.edu.gt>
2024-08-14 16:55:09 -0600

    Feat: Refactor for lab4

---
+++ Lab4/Ejercicio1/Node.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+#Clase Node cita: 
+#tutorialspoint . (2024). tutorialspoint. 
+# Obtenido de Python - Binary Tree: https://www.tutorialspoint.com/python_data_structure/python_binary_tree.htm
+class Node:
+    def __init__(self, data, id):
+        self.left = None
+        self.right = None
+        self.data = data
+        self.id = id  #se crea un id porque si agregamos el mismo nombre al nodo se une a varios lados del arbol
+        #cada valor es unico en la cadena
+
+#algoritmo de creacion 
+def build_tree(postfix):
+    stack = []
+    id_counter = 0 
+    for char in postfix:
+        if char.isalnum() or char == 'ε':  
+            node = Node(char, id_counter)  #solo agregamos el nodo
+            id_counter += 1
+            stack.append(node)
+        elif char in ['*', '|', ' ']:  
+            if char == '*':
+                node = Node(char, id_counter)
+                node.left = stack.pop()
+                id_counter += 1
+            elif char in ['|', ' ']:
+                node = Node(char, id_counter)
+                node.right = stack.pop() 
+                node.left = stack.pop()
+                id_counter += 1
+            stack.append(node)
+            #se hace esto porque * es de mayor presedencia que or. Y se aplicadirectamente a un valor unico
+    return stack[0]
--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# Laboratorios_Teoria_Computacion-
-
-## Laboratorios Link
-[Laboratorio 2](./Lab2/README.md)
-
-[Laboratorio 3](./Lab3/README.md)
-
-[Laboratorio 4](./Lab4/README.md)
+# Laboratorio 4
