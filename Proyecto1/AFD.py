23b5250001b58c22f8dc723149ef53072267893b
cor22982 <cor22982@uvg.edu.gt>
2024-09-09 17:50:50 -0600

    feat: Complete algoritm subset

--- Proyecto1/AFD.py
+++ Proyecto1/AFD.py
@@ -11,8 +11,6 @@ class AFD:
     self.S_ = []
     self.F_ = []
     self.Q_=  []
-    self.DfAD_states = []
-    self.DfAD_states.append(AFN.epsilon_closure({AFN.q0}))
     self.Q_.append(self.q0)
     self.state_count = 0  
 
@@ -39,32 +37,27 @@ class AFD:
 
 
 class Estado_AFD:
-    def __init__(self, numero, aceptacion=False, estados_AFN = []):
+    def __init__(self, numero, estados_AFN = []):
         self.numero = numero
-        self.aceptacion = aceptacion
         self.estados_AFN = estados_AFN
 
-    def set_to_Acept(self):
-        self.aceptacion = True
-
-class Transicion_AFD:
-    def __init__(self, initialstate, finalstate, valor):
-        self.q0 = initialstate
-        self.qf = finalstate
-        self.valor = valor
 
 def subset_Algoritm(AFN):
   afd = AFD(AFN)
    #algoritmo creacion de subconjuntos
   for s in AFN.Alfabeto:
-    # saco de la pila a T
-    T = afd.DfAD_states.pop()
-    # hago el regorido en el AFN
-    R = AFN.epsilon_closure(AFN.move(T, s))
-    # luego lo vuelvo a meter
-    afd.DfAD_states.append(T)
-    if R not in afd.DfAD_states:
-       afd.DfAD_states.append(R)
+    for T in afd.Q_:
+       R = AFN.epsilon_closure(AFN.move(T.estados_AFN, s))
+       estados_existentes = [i.estados_AFN for i in afd.Q_]
+       if R not in estados_existentes:
+          afd.state_count += 1
+          new_state = Estado_AFD(afd.state_count, estados_AFN=R)
+          # si existe un estado que es de aceptacion entonces lo hacemos de aceptacion
+          for estado_afn in R:
+             if estado_afn == AFN.F:
+                afd.F_.append(new_state)
+          afd.Q_.append(new_state)
+          afd.S_.append(Transicion(T, new_state, s))
   return afd
 
 
