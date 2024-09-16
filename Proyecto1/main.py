1ae568639d91c82b229d4f66a2001dccc79d4f34
cor22982 <cor22982@uvg.edu.gt>
2024-09-16 17:58:56 -0600

    feat> Add validate not hcaracters

--- Proyecto1/main.py
+++ Proyecto1/main.py
@@ -38,49 +38,49 @@ st.session_state.chain_w = st.text_input('Ingresa la cadena w para evaluar', st.
 
 # Evaluar cadena
 if st.button('Evaluar cadena en AFD Y AFN'):
-    if expression and st.session_state.chain_w:
-        regex = expression
-        postfix, _ = infixToPostfix(regex)
-        root = build_tree(postfix)
-        afn = buildAFN(root)
-        afd = subset_Algoritm(afn)
-        afd.minimizumAFD()
-        AFN_value = afn.acept_Chain(st.session_state.chain_w)
-        AFD_value = afd.acept_Chain(st.session_state.chain_w)
+    expression = expression or ''
+    regex = expression
+    postfix, _ = infixToPostfix(regex)
+    root = build_tree(postfix)
+    afn = buildAFN(root)
+    afd = subset_Algoritm(afn)
+    afd.minimizumAFD()
+    AFN_value = afn.acept_Chain(st.session_state.chain_w)
+    AFD_value = afd.acept_Chain(st.session_state.chain_w)
 
-        if AFN_value:
-            st.success("La cadena w es aceptada por el autómata AFN")
-        else:
-            st.error("La cadena w no es aceptada por el autómata AFN")
+    if AFN_value:
+        st.success("La cadena w es aceptada por el autómata AFN")
+    else:
+        st.error("La cadena w no es aceptada por el autómata AFN")
 
-        if AFD_value:
-            st.success("La cadena w es aceptada por el autómata AFD")
-        else:
-            st.error("La cadena w no es aceptada por el autómata AFD")
+    if AFD_value:
+        st.success("La cadena w es aceptada por el autómata AFD")
+    else:
+        st.error("La cadena w no es aceptada por el autómata AFD")
 
 # Generar AFD, AFN
 if st.button('Generar AFD, AFN'):
-    if expression:
-        try:
-            regex = expression
-            postfix, _ = infixToPostfix(regex)
-            root = build_tree(postfix)
-            draw_tree_graphviz(root)
-            st.text('Arbol sintactico generado')
-            st.image("\Laboratorios_Teoria_Computacion-\Proyecto1\\binary_tree_image.png", width=300)
-            afn = buildAFN(root)
-            afn.graphicAFN()
-            st.text('AFN de la expresión')
-            st.image("\Laboratorios_Teoria_Computacion-\Proyecto1\AFN_automata.png", width=800)
-            
-            afd = subset_Algoritm(afn)
-            afd.graphicAFD()
-            afd.minimizumAFD()
-            afd.graphicminimizumAFD()
-            
-            st.text('AFD de la expresión')
-            st.image('\Laboratorios_Teoria_Computacion-\Proyecto1\AFD_automata.png')
-            st.text('AFD minimizado de la expresión')
-            st.image('\Laboratorios_Teoria_Computacion-\Proyecto1\AFD_automata_minimizum.png')
-        except:
-            st.error("No se pudo generar los automatas , verifique que su expresion sea correcta")
+    expression = expression or ''
+    try:
+        regex = expression
+        postfix, _ = infixToPostfix(regex)
+        root = build_tree(postfix)
+        draw_tree_graphviz(root)
+        st.text('Arbol sintactico generado')
+        st.image("\Laboratorios_Teoria_Computacion-\Proyecto1\\binary_tree_image.png", width=300)
+        afn = buildAFN(root)
+        afn.graphicAFN()
+        st.text('AFN de la expresión')
+        st.image("\Laboratorios_Teoria_Computacion-\Proyecto1\AFN_automata.png", width=800)
+        
+        afd = subset_Algoritm(afn)
+        afd.graphicAFD()
+        afd.minimizumAFD()
+        afd.graphicminimizumAFD()
+        
+        st.text('AFD de la expresión')
+        st.image('\Laboratorios_Teoria_Computacion-\Proyecto1\AFD_automata.png')
+        st.text('AFD minimizado de la expresión')
+        st.image('\Laboratorios_Teoria_Computacion-\Proyecto1\AFD_automata_minimizum.png')
+    except:
+        st.error("No se pudo generar los automatas , verifique que su expresion sea correcta")
