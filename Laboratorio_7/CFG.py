b40d2cfd8a4d2f23f3f44051b63785200a1151fc
cor22982 <cor22982@uvg.edu.gt>
2024-10-02 02:52:12 -0600

    feat: Add cfg build

--- Laboratorio_7/CFG.py
+++ Laboratorio_7/CFG.py
@@ -1,6 +1,29 @@
+from Validators import *
+from Regex import Regex
 class CFG(object):
-  def __init__(self, inicial):
+  def __init__(self, regx):
+    self.regex = regx.gramatica
     self.T = []
     self.V = []
     self.P =[]
-    self.S = inicial
+    self.S = regx.gramatica[0][0]
+    self.build_CFG()
+
+  def build_CFG(self):
+    for produccions in self.regex:
+      for production in produccions:
+        for simbol in production:
+          if validateNonTerminal(simbol) and simbol not in self.V:
+            self.V.append(simbol)
+          elif validateTerminal(simbol) and simbol not in self.T:
+            self.T.append(simbol)
+          
+
+try:
+  regx = Regex()
+  regx.load_filename('Laboratorio_7\\file.txt')
+  regx.validateChains()
+  cfg = CFG(regx)
+  print(cfg)
+except ValueError as e:
+  print(f"Se produjo un error :  {e}")
--- Laboratorio_7/Validators.py
+++ Laboratorio_7/Validators.py
@@ -4,4 +4,4 @@ def validateNonTerminal (text):
   return text.isupper() and text.isalpha()
 
 def validateTerminal (text):
-  return not validateNonTerminal(text) and text not in operadores
+  return not validateNonTerminal(text) and text not in operadores and text != '→'
