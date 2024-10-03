0735b01208e594868c4729e91252466ab7156152
cor22982 <cor22982@uvg.edu.gt>
2024-10-02 20:15:13 -0600

    feat: Complete epsilon delete transitions

--- Laboratorio_7/CFG.py
+++ Laboratorio_7/CFG.py
@@ -1,4 +1,5 @@
 from Validators import *
+import copy
 from Regex import Regex
 class CFG(object):
   def __init__(self, regx):
@@ -22,23 +23,52 @@ class CFG(object):
           elif validateTerminal(simbol) and simbol not in self.T:
             self.T.append(simbol)
   
+  def buscar_produccion(self, nonterminal, terminal):
+        for produccion in self.P:
+            if produccion.v_ == nonterminal and produccion.t_ == terminal:
+                return True  
+  
+  def generar_combinaciones(self, produccion, anulables):
+    combinaciones = set()
+    combinaciones.add(produccion)
+    for simbolo in produccion:
+        if simbolo in anulables:
+            nueva_produccion = produccion.replace(simbolo, '', 1)
+            combinaciones.add(nueva_produccion)
+    return list(combinaciones)
+
   def quit_epsilon(self):
-    anulables = []
+    copy_productions = copy.deepcopy(self.P)
+    #lo hacemos set para que no se dupliquen
+    anulables = set()
     #verificamos los que tienen epsilon
     for produccion in self.P:
       if produccion.v_ in self.V and produccion.t_ == 'ε':
-        anulables.append(produccion.v_)
+        anulables.add(produccion.v_)
     #ahora verificamos aquellas que nos llevan a las anulables
     cambios = True
     while cambios:
       cambios = False
       for produccion in self.P:
-          if [simbolo for simbolo in anulables for simbolo in produccion.t_] and produccion.v_ not in anulables:
-              anulables.append(produccion.v_)
+          if all(simbolo in anulables for simbolo in produccion.t_) and produccion.v_ not in anulables:
+              anulables.add(produccion.v_)
               cambios = True
-
-    print(anulables)
-
+    #ahora lo que vamos a Eliminar las producciones epsilonn anulables
+    anulables = list(anulables)
+    for i, prod in enumerate(self.P):
+      if prod.v_ in anulables and prod.t_ == 'ε':
+        copy_productions.pop(i)
+      #ahora si alguna produccion tiene simbolos anulables entonces vamos a generar las nuevas producciones 
+      elif any(simbolo in prod.t_ for simbolo in anulables):
+        lista = self.generar_combinaciones(produccion=prod.t_, anulables=anulables)
+        for comb in lista:
+          if (comb != '') and (not self.buscar_produccion(nonterminal=prod.v_ , terminal=comb)):
+            produccion_combinacion = Production(nonterminal=prod.v_, terminal=comb)
+            copy_productions.append(produccion_combinacion)
+    # Aqui ya tenemos las producciones sin epsilon ahora las igualamos
+    self.P =[]
+    self.P = copy_productions
+    
 class Production(object):
   def __init__(self, nonterminal, terminal):
     self.v_ = nonterminal
@@ -50,6 +80,5 @@ try:
   regx.validateChains()
   cfg = CFG(regx)
   cfg.quit_epsilon()
-  print(cfg)
 except ValueError as e:
   print(f"Se produjo un error :  {e}")
