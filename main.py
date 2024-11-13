898318b0bfb922b0aad9329c9e706600cd2601cf
cor22982 <cor22982@uvg.edu.gt>
2024-11-13 02:10:24 -0600

    feaT: Ejercicio1

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 #Laboratorio 12 MATHEW ALEXANDER CORDERO AQUINO
 
 
-# EJERCICIO 2
+# EJERCICIO 1
+D = [
+    {'make': 'Nokia', 'model': 216, 'color': 'Black'},
+    {'make': 'Apple', 'model': 2, 'color': 'Silver'},
+    {'make': 'Huawei', 'model': 50, 'color': 'Gold'},
+    {'make': 'Samsung', 'model': 7, 'color': 'Blue'}
+]
 
+elementos_ordenados= sorted(D, key=lambda x: x['model'])
+print(elementos_ordenados)
+
+
+# EJERCICIO 2
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 nueva_lista = list(map(lambda x: x ** 3, lista))
 print(nueva_lista)
