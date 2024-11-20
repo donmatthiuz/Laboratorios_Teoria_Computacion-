bdd3734b4b71bd606bcb9a5f4ae1c0bb587a3c68
cor22982 <cor22982@uvg.edu.gt>
2024-11-20 12:12:11 -0600

    fix: Fix reconize

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from reader import Reader
 import yaml
 from TM import TM
-with open("files\machine_turing_aceptacion.yaml", "r") as file:
+with open("files\machine_turing_recognize.yaml", "r") as file:
     data = yaml.safe_load(file)
 lector = Reader(content=data)
 print(lector)
@@ -11,10 +11,10 @@ tm = TM(lector=lector)
 print(tm)
 print(tm.transiciones)
 tm.graph()
-valor, historial = tm.simulate(lector.cadenas[2])
+valor, historial = tm.simulate(lector.cadenas[3])
 
 
 for p in historial:
     print(p)
 
-print(valor)
+print(valor) 
